5410316b6dbda290b488964349134d3871007671
matteuskuns-cyber <matteus.kuns@escola.pr.gov.br>
2026-03-25 11:36:58 +0000

    Finalização de objetos js

---
+++ objetos/obj-01.js
@@ -0,0 +1,14 @@
+// Objetos são estrutura chabe-valor
+//Também chamadas de "key-value"
+// objetod podem conter : arrays, functions...
+// possuem metodos proprios
+
+const jogador = {
+    nome: "lielson",
+    gols: 1.985,
+    time: ["flamengo", "vasco", "santos"],
+}
+console.log(jogador.nome)
+console.log(jogador.gols)
+console.log(jogador.time[1])
+
---
+++ objetos/obj-02.js
@@ -0,0 +1,20 @@
+// apartir do objeto de exemplo crie
+// um objto de sua escolha
+// deve conter pelomenos 6 propriedades
+// imprima de um jeito amigavel
+
+const carro = {
+    nome: "jaguar E-taype 72",
+    concesionaria: "jaguar cars ltd",
+    ano: 1972,
+    genero: "esportivo",
+    elenco: ["jaguar E-taype 72", " jaguar suv F-Pace", "jaguar sedan XF"],
+    motor: "4.2L inline-6",
+}
+
+console.log(carro.nome);
+console.log(carro.concesionaria);
+console.log(carro.ano);
+console.log(carro.genero);
+console.log(carro.elenco[0]);
+console.log(carro.motor);
