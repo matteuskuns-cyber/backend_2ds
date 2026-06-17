27a4f4a66768125d63ff9bd7b74572f4f907730a
matteuskuns-cyber <matteus.kuns@escola.pr.gov.br>
2026-06-17 11:38:51 +0000

    finalizado o mod-chalk

--- 2ºtrimestre/modulos/fs/mod-fs-path.js
+++ 2ºtrimestre/modulos/fs/mod-fs-path.js
@@ -1,15 +1,14 @@
 
 const fs = require('fs')
 const path = require('path')
-const chalk = require('chalk').default
+const chalk = require('chalk').default //padrao
 
 try {
     //             Módulo-fs                  diretório    arquivo  codificação  
-    const arquivo = fs.readFileSync(path.join(__dirname, './festa.md'), 'utf-8')
-console.log(chalk.green('Arquivo carregado com sucesso!'))
+    const arquivo = fs.readFileSync(path.join(__dirname, './festamd'), 'utf-8')
     console.log(arquivo)
-console.log(chalk.red("Deu ruin:"))
+console.log(chalk.green("Arquivo carregado com sucesso"))
 } catch (erro) {
     console.log(erro.messagem)
+console.log(chalk.italic.bgBlue.bold.strikethrough.cyan.dim.inverse("Deu ruim:"))
 }
-
